4b44d947162f6416bc73a76f6e9745f5671d3a0d
SSahoo <srikant.sahoo1234@gmail.com>
2024-01-17 10:31:33 +0530

    seq_lib has updated

--- seq_lib.sv
+++ seq_lib.sv
@@ -1,13 +1,21 @@
 class base_lib extends uvm_sequence;
 	`uvm_object_utils(base_lib)
-	
+	uvm_phase phase;
+
 	function new(string name="", uvm_component parent=null);
 		super.new(name, parent);
 	endfunction
 	
 	task pre_body();
 		if(phase != null)begin
-			phase.raise_objection
+			phase.raise_objection(this);
+			phase.phase_done.set_drain_time(this, 50);
+		end
+	endtask
+
+	task post_body();
+		if(phase != null) begin
+			phase.drop_objection(this);
 		end
 	endtask
 endclass
